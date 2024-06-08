3d767db7cb079731f66ce57007c88e62183204ba
kandulatarunreddy <42193173+kandulatarunreddy@users.noreply.github.com>
2024-06-08 07:22:50 +0000

    1

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Hurray Finally Automated the project using Jenkins CI/CD Pipeline
         </a>
       </header>
       <p>
